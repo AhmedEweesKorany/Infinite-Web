af0d077abd765acb35e9fec295b078edbfb576db
Ahmed Ewees Korany <120169949+AhmedEweesKorany@users.noreply.github.com>
2024-01-07 14:28:52 +0200

    Revert "3ra2y ya negm "

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,40 +1,13 @@
 import React from 'react'
-import Side from './Side'
-import im from "./ronaldo.jpg"
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import "./products.css"
 function Products() {
-  let pro=[
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-    {title:"Ronaldo",text:"CR7"},
-  ]
   return (
-    <div>
-        <Side />
-        <div className='products'>
-              <div className="row">
-                  <img src={im} className='top-img' alt="" />
-              </div>
-              <div className="row" >
-                {
-                  pro.map((item)=>{
-                    return(
-                      <div className="col-3 mt-5">
-                        <img src={im} alt="" className="card-img" />
-                            <h6 className="title mt-2">{item.title}</h6>
-                            <span className="span price">{item.text}</span>
-                      </div>
-                    )
-                  })
-                }
-              </div>
-        </div>
+    <div className='products'>
+
+      {/* code el products hena ya 3ra2yyyyyyyy
+
+          ######### beltawfeee2 ya 7b
+      */}
     </div>
   )
 }
