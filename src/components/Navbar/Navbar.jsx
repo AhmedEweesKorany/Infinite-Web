218e79b523be810905be6bb22e7f676ffe84e132
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-01-07 17:42:53 +0200

    fix bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import "./navbar.css"
 import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
+import "./navbar.css"
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="container">
-            <div className="left">
+            <div className="left">  
             <div className="item">
                 <Link to="/products/1">Men</Link>
 
